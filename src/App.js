b788f5c32ff87624c93365c3a5e7db2262d330df
Eunah Kim <Eunahkim92@gmail.com>
2020-08-06 21:56:45 -0400

    Add Class to app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
 import Header from "./components/Header";
-import ResultsContainer from "./components/ResultsContainer";
+import Wrapper from "./components/Wrapper";
+import SearchBar from "./components/SearchBar";
+import Table from "./components/Table";
+import TableHeader from "./components/TableHeader";
+import TableRow from "./components/TableRow";
+import API from "./utils/API";
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <ResultsContainer />
-    </div>
-  );
+class App extends Component {
+  state = {
+    search: "",
+    results: []
+  };
+
+  componentDidMount() {
+      API.getEmployees()
+          .then(res => this.setState({ results: res.data.results }))
+          .catch(err => console.log(err));
+  };
+  
+  handleInputChange = event => {
+      const { name, value } = event.target;
+      this.setState({ [name]: value });
+  };
+
+  render() {
+      return (
+          <div>
+            <Header />
+              <Wrapper>
+                <SearchBar
+                search={this.state.search}
+                handleInputChange={this.handleInputChange}
+                />
+                <Table>
+                  <TableHeader />
+                  <TableRow results={this.state.results}/>
+                </Table>
+              </Wrapper>
+          </div>
+      );
+  }
 }
 
 export default App;
