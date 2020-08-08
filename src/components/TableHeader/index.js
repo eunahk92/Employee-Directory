bb074b53606598f2ec02caf830c69f7658cb1ffa
Eunah Kim <Eunahkim92@gmail.com>
2020-08-08 01:58:38 -0400

    Add sortNames method prop

--- src/components/TableHeader/index.js
+++ src/components/TableHeader/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
-const TableHeader = () => {
+const TableHeader = props => {
 	return (
         <thead className="thead-light">
             <tr>
                 <th scope="col">Picture</th>
-                <th scope="col">Name</th>
+                <th scope="col">Name 
+                    <i type="button" className="fas fa-caret-down" onClick={() => props.sortNames()}></i></th>
                 <th scope="col">Phone Number</th>
-                <th scope="col">Email</th>
-                <th scope="col">DOB</th>
+                <th scope="col">Email <i className="fas fa-caret-down"></i></th>
+                <th scope="col">DOB <i className="fas fa-caret-down"></i></th>
             </tr>
         </thead>
 	);
