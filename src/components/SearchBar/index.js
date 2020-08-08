3600f26679821e02c976a532ec311b728d58dddb
Eunah Kim <Eunahkim92@gmail.com>
2020-08-08 01:57:38 -0400

    Add keypress method to preventdefault on enter button

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-const SearchBar = ({ handleInputChange, search }) => (
+const SearchBar = ({ handleInputChange, search, handleKeyPress }) => (
   <form className="form-inline mb-3">
     <div className="form-group mb-2 mx-auto">
       <label htmlFor="search">Search For Employee:</label>
         <input
           onChange={handleInputChange}
+          onKeyPress={handleKeyPress}
           value={search}
           name="search"
           type="text"
